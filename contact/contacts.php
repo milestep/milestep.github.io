ea508867bc36fa2909a4cae6466a9374b982d741
Oleksandr <targonsky97@gmail.com>
2017-07-04 09:41:25 +0300

    form

--- contact/contacts.php
+++ contact/contacts.php
@@ -1,9 +1,9 @@
 <?php
 if($_POST)
     {
-    $to = "targonsky97@gmail.com"; //куда отправлять письмо
-    $from = 'почта@mail.ru'; // от кого
-    $subject = "Проверка почты"; //тема
+    $to = "targonsky97@gmail.com"; 
+    $from = 'почта@mail.ru'; 
+    $subject = "Проверка почты"; 
     $message = 'fullname: '.$_POST['name'].'; email: '.$_POST['email'].';';
     $headers = "Content-type: text/html; charset=UTF-8 \r\n";
     $headers .= "From: <почта@mail.ru>\r\n";
